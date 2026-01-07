bdb9148d1ae4a7ca7039a5b693e567d6aebd4745
Rahsaan Pringle <Raalnan1@Outlook.com>
2026-01-07 08:53:32 -0500

    Added Nuget Installer for demo

---
+++ Pdfolio.Conversion.Library.Demo/NugetInstall.bat
@@ -0,0 +1,11 @@
+dotnet add package Microsoft.Extensions.Configuration
+dotnet add package Microsoft.Extensions.Configuration.Json
+dotnet add package Microsoft.Extensions.Configuration.EnvironmentVariables
+dotnet add package Microsoft.Extensions.DependencyInjection
+dotnet add package Microsoft.Extensions.Logging
+dotnet add package Microsoft.Extensions.Logging.Console
+dotnet add package Microsoft.Data.Sqlite
+
+
+dotnet restore
+dotnet build
